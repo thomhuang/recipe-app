df5bddd1d0d8ce25f33f62c60aea75fe2f59e5cf
thomhuang <thomaskhuang@ucla.edu>
2021-04-28 01:58:53 -0700

    Fixed web-scrape formatting
    Separates ingredients, recipe steps, and
     tags with a new line between each element rather than one entire string.

--- recipe_crawler/recipe_crawler/spiders/serious_eats_scraper.py
+++ recipe_crawler/recipe_crawler/spiders/serious_eats_scraper.py
@@ -1,36 +1,40 @@
 import scrapy
 
-#This is just preliminary work to scrape the necessary information off of the SeriousEats.com website. I used the most popular recipe page on the website according to them.
 
+# This is just preliminary work to scrape the necessary information off of the SeriousEats.com website.
+# I used the most popular recipe page on the website according to them.
 class ingredientsSpider(scrapy.Spider):
+    name = "recipe"  # We call our spider "recipe" to be referenced in the shell
 
-	name = "recipe" #We call our spider "recipe" to be referenced in the shell
+    start_urls = [
+        "https://www.seriouseats.com/recipes/2016/12/the-best-roast-potatoes-ever-recipe.html"]  # our URL to work with
 
-	start_urls = ["https://www.seriouseats.com/recipes/2016/12/the-best-roast-potatoes-ever-recipe.html"] #our URL to work with
+    def parse(self, response):
+        for elements in response.css(
+                "div.recipe-body.summary"):  # div.recipe-body.summary contains all the information we need
 
-	def parse(self, response):
+            # The information extracted below is self-explanatory
 
-		for elements in response.css("div.recipe-body.summary"): # div.recipe-body.summary contains all the information we need
-        
-            #The information extracted below is self-explanatory
+            title = response.css("h1.title.recipe-title.c-post__h1::text").get()
 
-			title = response.css("h1.title.recipe-title.c-post__h1::text").get()
+            author = response.css("a.name::text").get()
 
-			author = response.css("a.name::text").get()
+            ingredients = "\n".join(response.css("li.ingredient::text").getall()[1:])
+            # Returns string with each ingredient separated with a new line
 
-			ingredients = response.css("li.ingredient::text").getall() #Returns array
+            recipe_procedure = "\n".join(response.css("ol.recipe-procedures-list.instructions").xpath(
+                '//*[@id="recipe-wrapper"]/div/ol/li/div/p/text()').getall()[1:])
+            # Returns string with each step of the recipe separated with a new line
 
-			recipe_procedure = response.css("ol.recipe-procedures-list.instructions").xpath('//*[@id="recipe-wrapper"]/div/ol/li/div/p/text()').getall() #Returns array
-			
-			tags = response.css("a.tag::text").getall()
+            tags = "\n".join(response.css("a.tag::text").getall()[1:])
+            # Returns a string with each tag separated with a new line
 
-			yield{ #How we lay out our information in the outputted file
-				"title" : title,
-				"author" : author,
-				"ingredients" : ingredients,
-				"recipe procedure" : recipe_procedure,
-				"tags" : tags
-			}
+            yield {  # How we lay out our information in the outputted file
+                "title": title,
+                "author": author,
+                "ingredients": ingredients,
+                "recipe procedure": recipe_procedure,
+                "tags": tags
+            }
 
-			#to save as .csv, in your terminal: scrapy crawl recipe -o recipes.csv
-	
+    # to save as .csv, in your terminal: scrapy crawl recipe -o recipes.csv
