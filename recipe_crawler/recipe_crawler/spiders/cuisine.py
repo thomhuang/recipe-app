8c53c0ce31bf262077e36f2577b88287a57a237c
Shreesh29 <shreesh29@hotmail.com>
2021-04-28 21:34:01 -0700

    Added Cuisine Scraper

---
+++ recipe_crawler/recipe_crawler/spiders/cuisine.py
@@ -0,0 +1,41 @@
+import scrapy
+import pandas as pd
+import numpy as np
+from scrapy.linkextractors import LinkExtractor
+from bs4 import BeautifulSoup
+
+
+class cuisine_spider(scrapy.Spider):
+	
+	name = "cuisine" 
+
+	start_urls = ["https://www.seriouseats.com/recipes/topics/cuisine/american"] 
+
+	def parse(self, response):
+		section = response.css('section.c-cards.c-cards--3-wide')
+		articles = section.css('article')
+		for i in range(0,24):
+			url = articles[i].css('a').attrib['href']
+			yield scrapy.Request(url, callback = self.parse_recipe)
+
+
+	def parse_recipe(self, response):
+		for elements in response.css("div.recipe-body.summary"):
+			
+			title = response.css("h1.title.recipe-title.c-post__h1::text").get()
+
+			author = response.css("a.name::text").get()
+
+			ingredients = response.css("li.ingredient::text").getall() 
+
+			recipe_procedure = response.css("ol.recipe-procedures-list.instructions").xpath('//*[@id="recipe-wrapper"]/div/ol/li/div/p/text()').getall() 
+			
+			tags = response.css("a.tag::text").getall()
+
+			yield { 
+				"title" : title,
+				"author" : author,
+				"ingredients" : ingredients,
+				"recipe procedure" : recipe_procedure,
+				"tags" : tags
+			}
