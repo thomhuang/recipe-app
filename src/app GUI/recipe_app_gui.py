31cedc8be252011526b39d2d0d5c452ee94ec9f7
thomhuang <thomaskhuang@ucla.edu>
2021-05-23 16:41:07 -0700

    Updated GUI
    Created layout for + beautified recipe view window.
    
    Plan to add:
    - Nutrients analysis
    - Related recipes by SQL query by `Tags`

--- src/app GUI/recipe_app_gui.py
+++ src/app GUI/recipe_app_gui.py
@@ -1,5 +1,7 @@
 from PyQt5.QtSql import *
 from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
 
 class SearchWindow(QWidget):
     def __init__(self):
@@ -42,7 +44,6 @@ class SearchWindow(QWidget):
         self.title_window.search_text = self.search_text_edit.text()
 
         self.title_window.initSearch()
-
 
 class RecipeTitleWindow(QWidget):
     def __init__(self):
@@ -110,7 +111,7 @@ class RecipeTitleWindow(QWidget):
         self.recipe_window.recipe_title = cell_val
 
         self.title_view.hide()
-        self.recipe_window.initView()
+        self.recipe_window.view_recipe(self.recipe_window.obtain_recipe())
 
 
 class RecipeWindow(QWidget):
@@ -122,13 +123,10 @@ class RecipeWindow(QWidget):
         self.conn = QSqlDatabase.addDatabase("QSQLITE")
         self.conn.setDatabaseName("recipes.db")
 
-    def initView(self):
-        self.view_recipe()
-
-    def view_recipe(self):
+    def obtain_recipe(self):
         recipe_info_cmd = \
             f"""
-        SELECT R.`Recipe Title`, R.Author, R.Cuisine, R.Ingredients, R.`Recipe Procedure`, R.Tags
+        SELECT R.`Recipe Title`, R.Author, R.Cuisine, R.Ingredients, R.`Recipe Procedure`, R.Tags, R.'Website URL'
         FROM recipes R
         WHERE R.`Recipe Title` = "{self.recipe_title}"
         """
@@ -140,14 +138,93 @@ class RecipeWindow(QWidget):
         recipe_model = QSqlQueryModel()
         recipe_model.setQuery(recipe_query)
 
-        self.recipe_view = QTableView()
-        self.recipe_view.setModel(recipe_model)
+        return recipe_model
 
-        self.recipe_view.show()
+    def view_recipe(self, model):
+        # All values taken from QSqlQuery returned in obtain_recipe() function
+        recipe_title = model.record(0).value(0)  # Recipe title
+        recipe_author = model.record(0).value(1)  # Recipe author
+        recipe_cuisine = model.record(0).value(2)  # Recipe cuisine
+        recipe_ingredients = model.record(0).value(3)  # Recipe ingredients
+        recipe_steps = model.record(0).value(4)  # Recipe steps
+        self.recipe_tags = model.record(0).value(5)  # Recipe tags
+        recipe_website = model.record(0).value(6)  # Recipe website
 
-        def initUI():
-            self.setGeometry(0, 0, 900, 500)
-            self.show()
+        self.setGeometry(0, 0, 900, 700)
+
+        # Everything done below is pretty self explanatory; create label/textedit, populate, move, etc.
+
+        title = QLabel(self)
+        title.setText("")
+        title.setText(recipe_title)
+        title.setWordWrap(True)  # Enables word wrap for specific label
+        title.setFont(QFont('Arial', 20, QFont.Bold))
+        title.setAlignment(Qt.AlignCenter)
+        title.setFixedWidth(600)
+        title.move(150, 10)
+
+        author_label = QLabel("Author: ", self)
+        author_label.move(45, 90)
+        author_label.setFont(QFont('Arial', 10, QFont.Bold))
+
+        author = QLabel(self)
+        author.setText(recipe_author)
+        author.move(95, 90)
+        author.setFont(QFont('Arial', 10))
+
+        cuisine_label = QLabel("Cuisine: ", self)
+        cuisine_label.move(265, 90)
+        cuisine_label.setFont(QFont('Arial', 10, QFont.Bold))
+
+        cuisine = QLabel(self)
+        cuisine.setText(recipe_cuisine)
+        cuisine.move(320, 90)
+        cuisine.setFont(QFont('Arial', 10))
+
+        tag_label = QLabel("Tags: ", self)
+        tag_label.move(485, 90)
+        tag_label.setFont(QFont('Arial', 10, QFont.Bold))
+
+        tags = QLabel(self)
+        tags.setFixedWidth(330)
+        tags.setText(self.recipe_tags)
+        tags.setWordWrap(True)
+        tags.move(525, 90)
+        tags.setFont(QFont('Arial', 10))
+
+        ingredients_label = QLabel("Ingredients: ", self)
+        ingredients_label.setFont(QFont('Arial', 10, QFont.Bold))
+        ingredients_label.move(45, 140)
+
+        ingredients = QTextEdit(self)
+        ingredients.setText(recipe_ingredients)
+        ingredients.setFixedWidth(200)
+        ingredients.setFixedHeight(300)
+        ingredients.move(30, 170)
+        ingredients.setFont(QFont('Arial', 10))
+
+        recipe_label = QLabel("Recipe Procedure: ", self)
+        recipe_label.setFont(QFont('Arial', 10, QFont.Bold))
+        recipe_label.move(265, 140)
+
+        recipe = QTextEdit(self)
+        recipe.setText(recipe_steps)
+        recipe.setFixedWidth(610)
+        recipe.setFixedHeight(300)
+        recipe.move(250, 170)
+        recipe.setFont(QFont('Arial', 10))
+
+        website_url_label = QLabel("If you'd like to see full details of this recipe click here: ", self)
+        website_url_label.setFont(QFont('Arial', 10, QFont.Bold))
+        website_url_label.move(35, 510)
+
+        website_url = QLabel(self)
+        website_url.setText('<a href="' + recipe_website + '/">' + recipe_title + '</a>')  # Creates hyperlink
+        website_url.setOpenExternalLinks(True)  # Allows us to click on hyperlink
+        website_url.move(385, 510)
+        website_url.setFont(QFont('Arial', 10))
+
+        self.show()
 
 
 def main():
