7c0ee92a859ee6d3b169b1290c22dbffde78ce5a
thomhuang <thomaskhuang@ucla.edu>
2021-05-31 14:40:02 -0700

    Cleaned repository

--- src/app GUI/recipe_app_gui.py
+++ src/app GUI/recipe_app_gui.py
@@ -4,6 +4,7 @@ from PyQt5.QtWidgets import (QWidget, QApplication, QMenuBar, QLabel, QComboBox,
 from PyQt5.QtCore import (Qt)
 from PyQt5.QtGui import (QFont)
 
+
 class SearchWindow(QWidget):
     def __init__(self):
         super().__init__()
